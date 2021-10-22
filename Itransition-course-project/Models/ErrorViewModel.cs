8bb2ee8571918ec6d195e7f7982887ceeb0ef08f
cheeze <maksprofy51@gmail.com>
2021-10-22 16:33:11 +0300

    *Main page *Nav bar

---
+++ Itransition-course-project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Itransition_course_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
