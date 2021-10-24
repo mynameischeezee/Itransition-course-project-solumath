175d51dc840a4c499ec3a49f504273fb8ba3fdc5
cheeze <maksprofy51@gmail.com>
2021-10-25 01:56:31 +0300

    * Fixed critical sql error bug

--- Itransition-course-project/Data/ApplicationDbContext.cs
+++ Itransition-course-project/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Itransition_course_project.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
